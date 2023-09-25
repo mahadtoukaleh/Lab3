3b63fd8e85bfd81a0b5523d55b65abeb183301ee
Mahad <94094060+mahadtoukaleh@users.noreply.github.com>
2023-09-25 19:05:46 -0400

    Update AddressBook.java
    Testing pull fromm git.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -21,5 +21,6 @@ public class AddressBook {
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
+        System.out.println("Testing the pull from Git.");
     }
 }
